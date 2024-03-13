0e6c2d1d55ae1ffd0c8608f84bbaa52a2a371700
Arvin <arvinrajabi004@gmail.com>
2024-03-13 22:34:55 +0330

    Add files via upload

---
+++ github-follower.py
@@ -0,0 +1,40 @@
+from github import Github
+import requests
+from time import sleep
+# PyGithub
+username = ""
+access_token = ""
+
+
+def follow_user(username, access_token, user_to_follow):
+    headers = {
+        "Authorization": f"token {access_token}"
+    }
+
+    url = f"https://api.github.com/user/following/{user_to_follow}"
+    
+    response = requests.put(url, headers=headers)
+
+    if response.status_code == 204:
+        print(f"Successfully followed {user_to_follow}")
+    else:
+        print(f"Error following {user_to_follow}: {response.text}")
+
+
+def followers(username, access_token):
+    github = Github(access_token)
+
+    user = github.get_user(username)
+    followers = user.get_followers()
+    for follower in followers:
+        user_to_follow = follower.login
+        try:
+            follow_user(username, access_token, user_to_follow)
+            print(f"Following {follower.login}")
+            sleep(1)
+
+        except :
+            print("Error following")
+
+
+followers(username, access_token)
